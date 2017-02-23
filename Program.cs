8ea1f6cddbd59a74cde8ecb3c1448f7e5358ac7d
gmzdev <gmzdev1986@gmail.com>
2017-02-23 21:42:50 +0800

    Initial

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new WebHostBuilder()
+                .UseKestrel()
+                .Configure(a => a.Run(c => c.Response.WriteAsync("Hello World!")))
+                .Build()
+                .Run();
+        }
+    }
+}
